8cab73253085b5d24d09637e0c5eb60246f7bd0a
Filipsys <filipsysak@gmail.com>
2024-11-29 00:48:02 +0100

    Added debug mode, first step checking using hashing for files & summary output

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,22 @@ import * as pathlib from "node:path";
 import fs from "node:fs/promises";
 import { existsSync } from "node:fs";
 import { createHmac } from "node:crypto";
+// import path from "node:path";
 
 const command = buildCommand({
     async func(this: CommandContext, _: {}, ...paths: string[]) {
         let inputContent: { pathname: string; content: string }[] = [];
         let inputHashes: { pathname: string; hash: string }[] = [];
+        let exactContents = new Array<string>();
+        let DEBUG_MODE = false;
 
+        // Check if the debug flag is enabled.
+        for (const [key] of Object.entries(_))
+            if (key === "debug") DEBUG_MODE = true;
+
+        // Hashing function to compare file contents.
+        // Uses SHA-256 to hash the content and returns
+        // the hash to be ready for comparison.
         const exactCheck = (data: string) => {
             return createHmac("sha256", data).digest("hex");
         };
@@ -18,25 +28,6 @@ const command = buildCommand({
             this.process.stdout.write("No input provided.\n");
             return;
         }
-
-        for (const path of paths) {
-            const index = paths.indexOf(path);
-
-            try {
-                const data = await fs.readFile(pathlib.join(process.cwd(), path), { encoding: "utf-8" });
-
-                inputHashes.push({
-                    pathname: path,
-                    hash: exactCheck(data)
-                })
-            } catch (err: any) {
-                this.process.stdout.write(`Error reading path ${index + 1}: ${err.message}\n`);
-            }
-        }
-
-        inputHashes.forEach((hash, index) => {
-            this.process.stdout.write(`Path ${index + 1}:\n${hash.pathname}\n${hash.hash}\n`);
-        });
 
         for (const path of paths) {
             const index = paths.indexOf(path);
@@ -58,14 +49,55 @@ const command = buildCommand({
                     pathname: path,
                     content: String.raw`${data}`,
                 });
+                inputHashes.push({
+                    pathname: path,
+                    hash: exactCheck(data)
+                });
             } catch (err: any) {
                 this.process.stdout.write(`Error reading path ${index + 1}: ${err.message}\n`);
             }
         }
 
-        inputContent.forEach((content, index) => {
-            this.process.stdout.write(`Path ${index + 1}:\n${content.pathname}\n${content.content}\n`);
+        // Comparing files to check for 100% similarity. This is
+        // a very rare case but should make the checking process
+        // faster with larger files that are identical.
+        inputContent.forEach((content) => {
+            const index = inputContent.indexOf(content);
+
+            for (let i = 0; i < inputContent.length; i++) {
+                if (i === index) continue;
+                if (exactCheck(content.content) !== inputHashes[i]?.hash) continue;
+
+                DEBUG_MODE
+                    ? this.process.stdout.write(`Path ${index + 1} does not differ from path ${i + 1}.\n`)
+                    : null;
+
+                if (!exactContents.includes(content.pathname)) {
+                    exactContents.push(content.pathname);
+                }
+            }
         });
+
+        DEBUG_MODE
+        ? (
+            inputContent.forEach((content) => {
+            this.process.stdout.write(`Path: ${content.pathname}\n`);
+            }),
+            exactContents.forEach((content) => {
+                this.process.stdout.write(`Exact: ${content}\n`);
+            })
+        ) : null;
+
+        // Outputting the comparison summary.
+        this.process.stdout.write("===[ Comparison summary ]===\n");
+        this.process.stdout.write(`Total paths checked: ${paths.length}\n`);
+        this.process.stdout.write(`Identical files: ${exactContents.length}\n`);
+        this.process.stdout.write(
+            exactContents.length > 0
+                ? `Paths: ${exactContents.join(", ")}\n`
+                : "No identical files found.\n"
+        );
+        this.process.stdout.write("\n");
     },
     parameters: {
         positional: {
@@ -75,11 +107,19 @@ const command = buildCommand({
                 parse: String,
             },
         },
+        flags: {
+            debug: {
+                kind: "boolean",
+                brief: "Enable debug mode",
+                optional: true,
+            }
+        },
     },
     docs: {
         brief: description,
         customUsage: [
             "/path/to/file /path/to/file ...",
+            "--debug /path/to/file /path/to/file ...",
             "/path/to/directory",
         ],
     }
